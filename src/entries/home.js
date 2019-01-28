bb6ceb28436ad33e2a5ee918eb56175d1ea2d9dd
Javier <f.castanedatrujillo@gmail.com>
2019-01-28 11:52:02 -0600

    modal 1

--- src/entries/home.js
+++ src/entries/home.js
@@ -5,8 +5,8 @@ import data from '../api.json';
 import Home from '../pages/containers/home';
 
 // console.log('Hola mundo!');
-const app = document.getElementById('app');
+const HomeContainer = document.getElementById('home-container');
 // const holaMundo = <h1>Hola mundo</h1>;
 
 // ReactDom.render(que voy a renderizar,donde lo haré);
-render(<Home data={data} />,app);
+render(<Home data={data} />,HomeContainer);
