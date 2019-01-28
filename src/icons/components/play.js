ee120b0976385cc453e25dd1f0644d6e94097c76
Javier <f.castanedatrujillo@gmail.com>
2019-01-28 08:47:36 -0600

    iconos

---
+++ src/icons/components/play.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Play() {
+  return (
+    <div>
+      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+        <title>play3</title>
+        <path d="M6 4l20 12-20 12z"></path>
+      </svg>
+    </div>
+  )
+}
+
+export default Play;
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media';
+import Play from '../../icons/components/play';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist
-    console.log(this.props.data);
+// class Playlist extends Component {
+//   render() {
+//     const playlist = this.props.data.categories[0].playlist
+//     console.log(this.props.data);
+//     return(
+//       <div>
+//         {
+//           playlist.map((item)=>{
+//             return <Media {...item} key={item.id}/>
+//           })
+//         }
+//       </div>
+//     )
+//   }
+// }
+function Playlist (props) {
+
+    const playlist = props.data.categories[0].playlist
+    console.log(props.data);
     return(
-      <div>
+      <div className="Playlist">
+        <Play />
         {
           playlist.map((item)=>{
             return <Media {...item} key={item.id}/>
@@ -14,7 +31,7 @@ class Playlist extends Component {
         }
       </div>
     )
-  }
+
 }
 
 export default Playlist;
