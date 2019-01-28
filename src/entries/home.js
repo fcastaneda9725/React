38b05d83c9e2cf99d4c9193e90238da80c703fb8
Javier <f.castanedatrujillo@gmail.com>
2019-01-28 10:30:51 -0600

    diseño de categorias

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import Playlist from './src/playlist/components/playlist';
-// import data from './src/api.json';
+import data from '../api.json';
 import Home from '../pages/containers/home';
 
 // console.log('Hola mundo!');
@@ -9,4 +9,4 @@ const app = document.getElementById('app');
 // const holaMundo = <h1>Hola mundo</h1>;
 
 // ReactDom.render(que voy a renderizar,donde lo haré);
-render(<Home />,app);
+render(<Home data={data} />,app);
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import Media from './media';
-import Play from '../../icons/components/play';
-import FullScreen from '../../icons/components/fullScreen';
-import Pause from '../../icons/components/pause';
-import Volume from '../../icons/components/volume';
+import './playlist.css'
 
-// class Playlist extends Component {
-//   render() {
-//     const playlist = this.props.data.categories[0].playlist
-//     console.log(this.props.data);
-//     return(
-//       <div>
-//         {
-//           playlist.map((item)=>{
-//             return <Media {...item} key={item.id}/>
-//           })
-//         }
-//       </div>
-//     )
-//   }
-// }
 function Playlist (props) {
-
-    const playlist = props.data.categories[0].playlist
-    console.log(props.data);
     return(
       <div className="Playlist">
-        <Play size={50} color="red"/>
-        <FullScreen size={50} color="blue" />
-        <Pause size={50} color="purple"/>
-        <Volume size={50} color="gold"/>
         {
-          playlist.map((item)=>{
+          props.playlist.map((item)=>{
             return <Media {...item} key={item.id}/>
           })
         }
