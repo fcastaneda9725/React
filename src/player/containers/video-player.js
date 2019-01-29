53cf05d4cb188a4e598f3b33f2ffe4eef400308d
Javier <f.castanedatrujillo@gmail.com>
2019-01-29 14:33:12 -0600

    cambios

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component {
+    state = {
+        pause: true,
+    }
+    togglePlay = (event) => {
+        this.setState({
+            pause: !this.state.pause
+        })
+    }
     render () {
         return (
             <VideoPlayerLayout>
                 <Title 
                 title="Video"
+                />
+                <PlayPause 
+                    handleClick={this.togglePlay}
+                    pause={this.state.pause}
                 />
                 <Video 
                 autoPlay={false}
